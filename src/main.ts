6bcc96326f75f432d23c2ed359d0398c206ec2a2
Arteandr <artedamatik@gmail.com>
2023-06-12 18:12:11 +0300

    Добавил пакет для работы свагера

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Инициализация сваггера
+  const config = new DocumentBuilder()
+    .setTitle('API для тестового задания')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
