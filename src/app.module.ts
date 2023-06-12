a9e451f2f7ac749769db6ddd144debdd57509f42
Arteandr <artedamatik@gmail.com>
2023-06-12 18:32:36 +0300

    Добавил TypeORM для удобной работы с БД

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BlogsModule } from './blogs/blogs.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [BlogsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "admin",
+      password: "admin",
+      database: "project",
+      entities: [],
+      synchronize: true,
+    }),
+    BlogsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
